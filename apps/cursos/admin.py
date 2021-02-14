17c458b45ae8474bbf3ae407451e53454c364b2b
ravellys <ravellyspyrrho@gmail.com>
2021-02-14 15:50:34 -0300

    instalação e configuração

---
+++ apps/cursos/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from apps.cursos.models import Curso, Avaliacao
+
+
+# Register your models here.
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('titulo', 'url', 'criacao', 'atualizacao', 'ativo')
+
+
+@admin.register(Avaliacao)
+class AvaliacaoAdmin(admin.ModelAdmin):
+    list_display = ('curso', 'nome', 'email', 'avaliacao', 'criacao', 'atualizacao', 'ativo')
