f320007cf2e284e9762247302aef5ffb4d20e399
ravellys <ravellyspyrrho@gmail.com>
2021-02-16 00:27:16 -0300

    sobreescrevendo metodos,
    utilizando get_queryset e get_object

--- apps/cursos/api/views_genericsview.py
+++ apps/cursos/api/views_genericsview.py
@@ -1,4 +1,6 @@
 from rest_framework import generics
+from rest_framework.generics import get_object_or_404
+
 from apps.cursos.api.serializers import AvaliacaoSerializer, CursoSerializer
 from apps.cursos.models import Avaliacao, Curso
 
@@ -13,6 +15,11 @@ class AvaliacoesAPIView(generics.ListCreateAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
 
+    def get_queryset(self):
+        if self.kwargs.get('curso_pk'):
+            return self.queryset.filter(curso_id=self.kwargs.get('curso_pk'))
+        return self.queryset
+
 
 # Detail, Delete e Update
 class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
@@ -23,3 +30,10 @@ class CursoAPIView(generics.RetrieveUpdateDestroyAPIView):
 class AvaliacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
+
+    def get_object(self):
+        if self.kwargs.get('curso_pk'):
+            return get_object_or_404(queryset=self.queryset,
+                                     curso_id=self.kwargs.get('curso_pk'),
+                                     id=self.kwargs.get('avaliacao_pk'))
+        return super().get_object()
