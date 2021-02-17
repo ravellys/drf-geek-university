95ae7a8386cf960cd2ae448a1f06119443a9537b
ravellys <ravellyspyrrho@gmail.com>
2021-02-17 00:49:48 -0300

    utilizando relações no DRF

--- apps/cursos/api/serializers.py
+++ apps/cursos/api/serializers.py
@@ -13,10 +13,18 @@ class AvaliacaoSerializer(serializers.ModelSerializer):
 
 
 class CursoSerializer(serializers.ModelSerializer):
+    # Nested relationship
+    # avaliacoes = AvaliacaoSerializer(many=True, read_only=True)
+
+    # Hyperlinked Related Field
+    # avaliacoes = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='avaliacao-detail')
+
+    # Primary Key Related Field
+    avaliacoes = serializers.PrimaryKeyRelatedField(many=True, read_only=True)
 
     class Meta:
         model = Curso
-        fields = ('id', 'titulo', 'url', 'criacao', 'ativo')
+        fields = ('id', 'titulo', 'url', 'criacao', 'ativo', 'avaliacoes')
 
 
 # Testando serializers
