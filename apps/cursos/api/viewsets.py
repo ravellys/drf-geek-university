de418ee45e1ae3a4174e0c02e1decb38a97e434b
ravellys <ravellyspyrrho@gmail.com>
2021-02-16 23:29:50 -0300

    Customizando viewsets.py

--- apps/cursos/api/viewsets.py
+++ apps/cursos/api/viewsets.py
@@ -1,6 +1,7 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, mixins
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
 
 from apps.cursos.api.serializers import CursoSerializer, AvaliacaoSerializer
 from apps.cursos.models import Curso, Avaliacao
@@ -17,7 +18,18 @@ class CursoViewSet(viewsets.ModelViewSet):
         return Response(serializer.data)
 
 
-class AvaliacaoViewSet(viewsets.ModelViewSet):
+# class AvaliacaoViewSet(viewsets.ModelViewSet):
+#     queryset = Avaliacao.objects.all()
+#     serializer_class = AvaliacaoSerializer
+
+
+class AvaliacaoViewSet(
+    mixins.CreateModelMixin,
+    mixins.RetrieveModelMixin,
+    mixins.UpdateModelMixin,
+    # mixins.DestroyModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet
+):
     queryset = Avaliacao.objects.all()
     serializer_class = AvaliacaoSerializer
-
